959fea1253cdbf7ca3cb78c6597b7ca3f444f3d1
IsuruKRanasundara <ranasundaraisuru4@gmail.com>
2026-04-09 11:32:45 +0530

    New Changes are there and going to be hosted in vercel

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import LoginPage from '../pages/auth/LoginPage';
+import RegisterPage from '../pages/auth/RegisterPage';
 
-const AppRoutes: React.FC = () => {
-  return <div>AppRoutes</div>;
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
 };
 
 export default AppRoutes;
