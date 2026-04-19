bb772a52f649616cffb7392fc4eb7303349320cd
IsuruKRanasundara <ranasundaraisuru4@gmail.com>
2026-04-19 11:27:17 +0530

    fix: remove hardcoded MongoDB URI and rely on environment variable

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const mongoUri = process.env.MONGO_URI||"mongodb+srv://ranasundaraisuru4_db_user:k0bE4rJTYzbAKMXe@cluster0.y8kpfa4.mongodb.net/?appName=Cluster0";
+  const mongoUri = process.env.MONGO_URI;
 
   if (!mongoUri) {
     throw new Error('MONGO_URI is not defined in environment variables');
